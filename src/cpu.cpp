4176a2ad20672d9da2070fade4c677a771941c71
leofracca <leonardofraccaroli1999@gmail.com>
2022-09-02 20:09:55 +0200

    Implemented new CPU arithmetic functions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -167,4 +167,88 @@ namespace gameboy
         m_registers->setFlag(HALF_CARRY_FLAG, (n & 0x0F) == 0x0F);
         // Carry flag not affected
     }
+
+    void CPU::add_hl(uint16_t nn)
+    {
+        unsigned int resultFull = m_registers->hl + nn; // Save the result in a temporary variable to check for carry from bit 15
+
+        // Zero flag not affected
+        // Set the subtract flag to 0
+        m_registers->setFlag(SUBTRACT_FLAG, false);
+        // Set the half-carry flag if there is a carry from bit 11
+        m_registers->setFlag(HALF_CARRY_FLAG, (m_registers->hl & 0x0FFF) + (nn & 0x0FFF) > 0x0FFF);
+        // Set the carry flag if there is a carry from bit 15
+        m_registers->setFlag(CARRY_FLAG, resultFull > 0xFFFF);
+
+        // Set the value of the register HL to the result
+        m_registers->hl = static_cast<uint16_t>(resultFull);
+    }
+
+    void CPU::add_sp(int8_t n)
+    {
+        unsigned int resultFull = m_registers->sp + n;
+
+        // Set the zero flag to 0
+        m_registers->setFlag(ZERO_FLAG, false);
+        // Set the subtract flag to 0
+        m_registers->setFlag(SUBTRACT_FLAG, false);
+        // Set the half-carry flag according to operation
+        m_registers->setFlag(HALF_CARRY_FLAG, ((m_registers->sp ^ n ^ (resultFull & 0xFFFF)) & 0x10) == 0x10);
+        // Set the carry flag according to operation
+        m_registers->setFlag(HALF_CARRY_FLAG, ((m_registers->sp ^ n ^ (resultFull & 0xFFFF)) & 0x100) == 0x100);
+
+        // Set the value of the register SP to the result
+        m_registers->sp = static_cast<uint16_t>(resultFull);
+    }
+
+    void CPU::ldhl(int8_t n)
+    {
+        unsigned int resultFull = m_registers->sp + n;
+
+        // Set the zero flag to 0
+        m_registers->setFlag(ZERO_FLAG, false);
+        // Set the subtract flag to 0
+        m_registers->setFlag(SUBTRACT_FLAG, false);
+        // Set the half-carry flag according to operation
+        m_registers->setFlag(HALF_CARRY_FLAG, ((m_registers->sp ^ n ^ (resultFull & 0xFFFF)) & 0x10) == 0x10);
+        // Set the carry flag according to operation
+        m_registers->setFlag(HALF_CARRY_FLAG, ((m_registers->sp ^ n ^ (resultFull & 0xFFFF)) & 0x100) == 0x100);
+
+        // Set the value of the register SP to the result
+        m_registers->hl = static_cast<uint16_t>(resultFull);
+    }
+
+    void CPU::swap(uint8_t &n)
+    {
+        // Swap the upper and lower nibbles of n
+        n = (n & 0x0F) << 4 | (n & 0xF0) >> 4;
+
+        // Set the zero flag if the result is 0
+        m_registers->setFlag(ZERO_FLAG, n == 0);
+        // Set the subtract flag to 0
+        m_registers->setFlag(SUBTRACT_FLAG, false);
+        // Set the half-carry flag to 0
+        m_registers->setFlag(HALF_CARRY_FLAG, false);
+        // Set the carry flag to 0
+        m_registers->setFlag(CARRY_FLAG, false);
+    }
+
+    void CPU::daa()
+    {
+        // TODO
+        // See https://en.wikipedia.org/wiki/Binary-coded_decimal
+    }
+
+    void CPU::cpl()
+    {
+        // Zero flag not affected
+        // Set the subtract flag to 1
+        m_registers->setFlag(SUBTRACT_FLAG, true);
+        // Set the half-carry flag to 1
+        m_registers->setFlag(HALF_CARRY_FLAG, true);
+        // Carry flag not affected
+
+        // Complement the value of the register A
+        m_registers->a = ~m_registers->a;
+    }
 }
