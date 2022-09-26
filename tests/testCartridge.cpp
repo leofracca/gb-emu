fd05f52ff1033574691e7a1c897cf2b04a6c15d2
leofracca <leonardofraccaroli1999@gmail.com>
2022-09-26 21:52:37 +0200

    Added some cartridge tests

---
+++ tests/testCartridge.cpp
@@ -0,0 +1,24 @@
+#include "../third_party/catch.hpp"
+#include "../include/cartridge.h"
+
+namespace gameboyTest
+{
+    using namespace gameboy;
+
+    const std::string TEST_ROM = "../tests/roms/cpu_instrs.gb";
+
+    const uint8_t TEST_ROM_TITLE[] = {0x43, 0x50, 0x55, 0x5f, 0x49, 0x4e, 0x53, 0x54, 0x52, 0x53};
+
+    TEST_CASE("Cartridge", "[cartridge]")
+    {
+        Cartridge cartridge;
+
+        cartridge.loadROM(TEST_ROM);
+
+        int j = 0;
+        for (int i = 0x0134; i < 0x0142; i++)
+        {
+            REQUIRE(cartridge.read(i) == TEST_ROM_TITLE[j++]);
+        }
+    }
+} // namespace gameboyTest
